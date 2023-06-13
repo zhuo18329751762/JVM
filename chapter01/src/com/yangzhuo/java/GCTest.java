0057cdfb0dc89e0529276d2ff7b0ca64d0ae0576
zhuo18329751762 <2199698862@qq.com>
2023-06-13 20:55:22 +0800

    GC日志查看

---
+++ chapter01/src/com/yangzhuo/java/GCTest.java
@@ -0,0 +1,22 @@
+package com.yangzhuo.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GCTest {
+    public static void main(String[] args) {
+        int i=0;
+        try{
+            List<String> list=new ArrayList<>();
+            String a="itcast.com";
+            while (true){
+                list.add(a);
+                a=a+a;
+                i++;
+            }
+        }catch (Throwable t){
+            t.printStackTrace();
+            System.out.println("遍历次数为: "+i);
+        }
+    }
+}
